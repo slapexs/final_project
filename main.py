b1dcb125a724ff0ee53cf8157d8f49cbe72ed218
slapexs <slapexs@gmail.com>
2022-11-27 14:47:03 +0700

    Sample more docs

--- main.py
+++ main.py
@@ -33,9 +33,11 @@ for detail in text:
 
 # sample
 tfidf_vectorizer = fnd_tfidf.create_tfidfvectorizer()
-tfidf_vector= tfidf_vectorizer.fit_transform(docs[100:106])
+tfidf_vector= tfidf_vectorizer.fit_transform(docs[10:106])
 tfidf_array = np.array(tfidf_vector.todense())
 
 #แปลงเป็น DataFrame เพื่อง่ายแก่การอ่าน
 df1 = pd.DataFrame(tfidf_array,columns=tfidf_vectorizer.get_feature_names())
-print(df1)
+
+# View top 10 ifidf score
+print(df1.apply(lambda s: s.nlargest(10).index.tolist(), axis=1).ravel())
