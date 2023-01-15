64920d88c793cd5a6b85a2e4efe3c1538f79505d
slapexs <slapexs@gmail.com>
2023-01-15 12:31:20 +0700

    :recycle::bug: Refactor code and fix bug filter detail in any cluster

--- tfidf_cosine_similarity.py
+++ tfidf_cosine_similarity.py
@@ -7,37 +7,52 @@ from pythainlp.corpus import thai_stopwords
 df = pd.read_csv('./document/clustered_company.csv')
 th_stopword = list(thai_stopwords())
 
-keyword = 'บริการลักษณะ Digital Solution (Marketing) Agency - Online marketing'
+keyword = 'บริการลักษณะ Digital Solution (Marketing)'
 
-cluster = 7
-def segment_and_clean_text(text):
-    words = word_tokenize(text, None, 'newmm', False)
-    cleaned_words = [word for word in words if word not in th_stopword]
-    return "".join(cleaned_words)
+class Calculate_cosinesim:
+    def __init__(self, keyword, amount_cluster:int, df) -> None:
+        self.keyword = keyword
+        self.cluster = amount_cluster
+        self.df = df
+    
+    # Filter detail by cluster
+    def filter_detail(self):
+        self.cluster_detail = []
+        for c in range(self.cluster):
+            temp = []
+            for i in range(len(self.df)):
+                if self.df.iloc[i]['cluster'] == c:
+                    temp.append(self.df.iloc[i]['detail'])
+            self.cluster_detail.append(temp)
+        return self.cluster_detail
+    
+    def segment_and_clean_text(self, text):
+        self.words = word_tokenize(text, None, 'newmm', False)
+        self.cleaned_words = [word for word in self.words if word not in th_stopword]
+        return "".join(self.cleaned_words)
+    
+    def cluster_wordseg(self):
+        self.temp = []
+        for c in range(self.cluster):
+            self.temp_detail = "".join(self.filter_detail()[c])
+            self.temp.append(self.segment_and_clean_text(self.temp_detail))
+            
+        return self.temp
 
-def calculate_socine(keyword, cluster_detail):
-    keyword_search = segment_and_clean_text(keyword)
-    detail = segment_and_clean_text(cluster_detail)
-    vectorizer = TfidfVectorizer()
-    # Fit the vectorizer to the tokenized documents
-    vectors = vectorizer.fit_transform([keyword_search, detail])
+    def calculate_cosine(self, detail):
+        self.keyword = self.segment_and_clean_text(self.keyword)
+        self.detail = self.segment_and_clean_text(detail)
+        self.vectorizer = TfidfVectorizer()
+        self.vector = self.vectorizer.fit_transform([self.keyword, self.detail])
+        self.similarity = cosine_similarity(self.vector, self.vector)
+        return self.similarity
 
-    # Calculate the cosine similarity between the two documents
-    similarity = cosine_similarity(vectors, vectors)
+    def response_cosine(self):
+        return [self.calculate_cosine(i)[0][1] for i in self.cluster_wordseg()]
 
-    return similarity[0][1]
 
-cluster_detail = []
-for c in range(cluster):
-    temp = ""
-    for i in range(len(df)):
-        if df.iloc[i]['cluster'] == c:
-            temp = "".join(df.iloc[i]['detail'])
-    cluster_detail.append(temp)
-
-cosine_values = []
-for k in range(len(cluster_detail)):
-    cosine_values.append(calculate_socine(keyword, cluster_detail[k]))
+obj = Calculate_cosinesim(keyword, 7, df)
+cosine_values = obj.response_cosine()
 print(cosine_values)
 print(f'max cosine similarity: {max(cosine_values)}')
 print(f'cluster: {cosine_values.index(max(cosine_values))}')
