0d7b920c796bcea57f3dea267c88a56c68f92cfc
slapexs <slapexs@gmail.com>
2023-03-31 12:58:20 +0700

    :package: Test file for write report

--- report/demo/main.py
+++ report/demo/main.py
@@ -1,6 +1,10 @@
-from pythainlp import word_tokenize
+from pythainlp.tokenize import word_tokenize
+import pandas as pd
 
-text = 'นอนตากลมดูดาว'
-
-result = word_tokenize(text, None, 'newmm', False)
-print(result)
+rawf = '../../file_test_accuracy/cleaned_companies.csv'
+raw_df = pd.read_csv(rawf)
+n = 190
+# cut_newmm = word_tokenize(raw_df.iloc[n]['รายละเอียดธุรกิจ'], None, 'newmm', False)
+# cut_longest = word_tokenize(raw_df.iloc[n]['รายละเอียดธุรกิจ'], None, 'longest', False)
+# cut_deepcut = word_tokenize(raw_df.iloc[n]['รายละเอียดธุรกิจ'], None, 'deepcut', False)
+print(raw_df.iloc[n]['รายละเอียดธุรกิจ'])
